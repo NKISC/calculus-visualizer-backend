7d306abcb4a13f39ede9e8de5f29cbc218161db7
EM <energymc@qq.com>
2026-06-03 19:59:06 +0800

    chore: structural modifications

--- app/math_engine/derivative.py
+++ app/math_engine/derivative.py
@@ -1,6 +1,6 @@
 import sympy as sp
 from sympy import Symbol, Derivative
-from backend.app.math_engine.parser import parse_expression
+from app.math_engine.parser import parse_expression
 
 def compute_derivative(expression: str, variable: str, num: int | None = 1) -> sp.Expr: # str输入一个字符串作为需要求导的公式，num不输入则求一阶导数，输入则求对应阶导
     f=parse_expression(expression)

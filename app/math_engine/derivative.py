1a55a04d5b3dc933828e4a3bf5393081c00a23b6
hfwsl <18883810038@163.com>
2026-06-04 12:15:04 +0800

    fix: the function of deriving a certain point

--- app/math_engine/derivative.py
+++ app/math_engine/derivative.py
@@ -1,9 +1,15 @@
 import sympy as sp
 from sympy import Symbol, Derivative
+
 from app.math_engine.parser import parse_expression
 
-def compute_derivative(expression: str, variable: str, num: int | None = 1) -> sp.Expr: # str输入一个字符串作为需要求导的公式，num不输入则求一阶导数，输入则求对应阶导
-    f=parse_expression(expression)
-    x=Symbol(variable)
-    d=Derivative(f,x, num)
-    return d.doit()
+
+def compute_derivative(expression: str, variable: str, num: int | None = 1,
+                       x_num: float | None = None) -> sp.Expr:  # str输入一个字符串作为需要求导的公式，num不输入则求一阶导数，输入则求对应阶导
+    f = parse_expression(expression)
+    x = Symbol(variable)
+    d = Derivative(f, x, num)
+    if x_num is None:
+        return d.doit()
+    else:
+        return d.doit().subs({x: x_num})

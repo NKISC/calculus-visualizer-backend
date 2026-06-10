9396f3c2b7578e18ddc963a011d53ed215a167a5
hfwsl <18883810038@163.com>
2026-06-10 21:34:35 +0800

    fix: When the function is not differentiable, it will return "NaN"

--- app/math_engine/derivative.py
+++ app/math_engine/derivative.py
@@ -1,15 +1,21 @@
-import sympy as sp
+import sympy
 from sympy import Symbol, Derivative
 
 from app.math_engine.parser import parse_expression
 
 
 def compute_derivative(expression: str, variable: str, num: int = 1,
-                       x_num: float | None = None) -> sp.Expr:  # str输入一个字符串作为需要求导的公式，num不输入则求一阶导数，输入则求对应阶导
+                       x_num: float | None = None) -> str | Derivative:  # str输入一个字符串作为需要求导的公式，num不输入则求一阶导数，输入则求对应阶导
     f = parse_expression(expression)
     x = Symbol(variable)
     d = Derivative(f, x, num)
+    d = d.doit()
+    d_str = str(d)
+    problematic = ['Derivative', 'nan', 'zoo']
+    if any(word in d_str for word in problematic):
+        return sympy.nan
+
     if x_num is None:
-        return d.doit()
+        return d
     else:
-        return d.doit().subs({x: x_num})
+        return d.subs({x: x_num})
